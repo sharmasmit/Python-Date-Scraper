787cdcff91a35cb85e31f55da8ef60612951f667
sharmasmit <smeet.sharma007@gmail.com>
2024-07-02 12:07:52 +0530

    DataClass Added

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello !")
+from playwright.sync_api import sync_playwright
+from dataclasses import dataclass, asdict, field
+import pandas as pd
+import argparse
+
+@dataclass
+class Business:
+    name: str = None
+    address: str = None
+    website: str = None
+    phone_number: str = None
